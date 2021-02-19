fd00d6b2b8e909118a59117a7f69a116f0dbdb6b
Ben Simms <ben@bensimms.moe>
2021-02-19 20:45:09 +0000

    Use repository bevy

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_ui.system())
         .run();
+}
+
+fn setup_ui(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(UiCameraBundle::default());
 }
