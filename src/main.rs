c5da07b5a4a2bd029e714c797aebcb97ea72235b
Ben Simms <ben@bensimms.moe>
2021-02-20 00:28:39 +0000

    Dragging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 
 mod button;
+mod dragging;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(button::ButtonPlugin)
+        .add_plugin(dragging::DragPlugin)
         .add_startup_system(setup_ui.system())
         .run();
 }
@@ -15,19 +17,24 @@ fn setup_ui(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(UiCameraBundle::default()).spawn(NodeBundle {
-        style: Style {
-            size: Size::new(Val::Px(200.0), Val::Percent(80.0)),
-            position_type: PositionType::Absolute,
-            position: Rect {
-                left: Val::Px(210.0),
-                bottom: Val::Px(10.0),
+    let jam_texture = asset_server.load("sprites/jam_jar.png");
+
+    commands
+        .spawn(UiCameraBundle::default())
+        .spawn(OrthographicCameraBundle::new_2d())
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Px(200.0), Val::Percent(80.0)),
+                border: Rect::all(Val::Px(20.0)),
                 ..Default::default()
             },
-            border: Rect::all(Val::Px(20.0)),
+            material: materials.add(Color::rgb(0.65, 0.65, 0.0).into()),
             ..Default::default()
-        },
-        material: materials.add(Color::rgb(0.65, 0.65, 0.0).into()),
-        ..Default::default()
-    });
+        })
+        .spawn(SpriteBundle {
+            material: materials.add(jam_texture.clone().into()),
+            ..Default::default()
+        })
+        .with(dragging::Hoverable)
+        .with(dragging::Draggable);
 }
